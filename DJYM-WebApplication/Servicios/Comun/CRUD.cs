c1def282603b5e681656c8cda823366978816df1
Johan Pérez <johanperezmarin@gmail.com>
2024-11-15 14:23:23 -0500

    Se mejora la claridad de los métodos dentro de 'CRUD'

--- DJYM-WebApplication/Servicios/Comun/CRUD.cs
+++ DJYM-WebApplication/Servicios/Comun/CRUD.cs
@@ -5,138 +5,114 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Web;
 
 namespace DJYM_WebApplication.Servicios.Comun
 {
     public class CRUD<TEntidad> where TEntidad : class, IEntidadConClavePrimaria
     {
-        private readonly DBSuper_DJYMEntities DJYM = new DBSuper_DJYMEntities();
-		public TEntidad Entidad { get; set; }
-
+        private readonly DBSuper_DJYMEntities DJYM;
+        public TEntidad Entidad { get; set; }
+        public CRUD()
+        {
+            DJYM = new DBSuper_DJYMEntities();
+        }
 
         public Resultado<TEntidad> Insertar()
         {
-			try
-			{
-                TEntidad entidadInsertada = DJYM.Set<TEntidad>().Add(Entidad);
-				DJYM.SaveChanges();
-
-				string mensajeExitoso = $"{typeof(TEntidad).Name} se inserta exitosamene: ";
-				Resultado<TEntidad> resultado = new Resultado<TEntidad>(entidadInsertada)
-				{
-					MensajeExito = mensajeExitoso,
-				};
-				return resultado;
-            }
-			catch (Exception ex)
-			{
-				return new Resultado<TEntidad>(ex.Message);
-			}
-		}
-
-		public Resultado<TEntidad> ConsultarXId()
-		{
-			try
-			{
-                TEntidad entidadConsultada = DJYM.Set<TEntidad>().Find(Entidad.ClavePrimaria);
-                if (entidadConsultada != null)
+            try
+            {
+                if (ConsultarXId().Exito)
                 {
-                    string mensajeExitoso = $"{typeof(TEntidad).Name} se consulta exitosamente";
-                    Resultado<TEntidad> resultado = new Resultado<TEntidad>(entidadConsultada)
-                    {
-                        MensajeExito = mensajeExitoso
-                    };
-                    return resultado;
+                    string mensajeError = $"{typeof(TEntidad).Name} ya existe en la base de datos";
+                    return new Resultado<TEntidad>(mensajeError);
                 }
-                else
+
+                DJYM.Set<TEntidad>().Add(Entidad);
+                DJYM.SaveChanges();
+
+                string mensajaExito = $"{typeof(TEntidad).Name} se insertó exitosamente";
+                return new Resultado<TEntidad>(Entidad) { MensajeExito = mensajaExito };
+            }
+            catch (Exception ex)
+            {
+                return new Resultado<TEntidad>(ex.Message);
+            }
+
+        }
+
+        public Resultado<TEntidad> ConsultarXId()
+        {
+            try
+            {
+                TEntidad entitidadConsultada = DJYM.Set<TEntidad>().Find(Entidad.ClavePrimaria);
+
+                if (entitidadConsultada == null)
                 {
                     string mensajeError = $"{typeof(TEntidad).Name} no existe en la base de datos";
                     return new Resultado<TEntidad>(mensajeError);
                 }
+                string mensajeExito = $"{typeof(TEntidad).Name} se consultó exitosamente";
+                return new Resultado<TEntidad>(entitidadConsultada) { MensajeExito = mensajeExito };
             }
-			catch (Exception ex)
-			{
-				return new Resultado<TEntidad>(ex.Message);
-			}
-		}
+            catch (Exception ex)
+            {
+                return new Resultado<TEntidad>(ex.Message);
+            }
+        }
 
         public Resultado<IQueryable> Consultar()
-		{
-			try
-			{
-                IQueryable<TEntidad> entidadQueryable = DJYM.Set<TEntidad>().AsQueryable();
-				string mensajeExito = $"L@s {typeof(TEntidad).Name}S se consultaron exitosamente";
-				Resultado<IQueryable> resultado = new Resultado<IQueryable>(entidadQueryable)
-				{
-					MensajeExito = mensajeExito
-				};
-				return resultado;
+        {
+            try
+            {
+                string mensajeExito = $"L@s {typeof(TEntidad).Name}S se consultaron exitosamente";
+                IQueryable entidadesQueryable = DJYM.Set<TEntidad>().AsQueryable();
+                return new Resultado<IQueryable>(entidadesQueryable) { MensajeExito = mensajeExito };
             }
-			catch (Exception ex)
-			{
-				return new Resultado<IQueryable>(ex.Message);
-			}
-		}
+            catch (Exception ex)
+            {
+                return new Resultado<IQueryable>(ex.Message);
+            }
+        }
 
-		public Resultado<TEntidad> Actualizar()
-		{
-			try
-			{
-				if (ConsultarXId().Exito)
-				{
-					DJYM.Set<TEntidad>().AddOrUpdate(Entidad);
-					DJYM.SaveChanges();
+        public Resultado<TEntidad> Actualizar()
+        {
+            try
+            {
+                Resultado<TEntidad> resultado = ConsultarXId();
+                if (!resultado.Exito)
+                    return resultado;
 
-                    TEntidad entidadActualizada = ConsultarXId().Value;
-					string mensajeExito = $"{typeof(TEntidad).Name} se actualiza exitosamente";
-					Resultado<TEntidad> resultado = new Resultado<TEntidad>(entidadActualizada)
-					{
-						MensajeExito = mensajeExito
-					};
-					return resultado;
-                }
-				else
-				{
-					string mensajeError = $"{typeof(TEntidad).Name} con identificador {Entidad.ClavePrimaria} no existe en la base de datos";
-					return new Resultado<TEntidad>(mensajeError);
-				}
-			}
-			catch (Exception ex)
-			{
-				return new Resultado<TEntidad>(ex.Message);
-			}
-		}
+                DJYM.Set<TEntidad>().AddOrUpdate(Entidad);
+                DJYM.SaveChanges();
 
-		public Resultado<TEntidad> Eliminar()
-		{
-			try
-			{
-				Resultado<TEntidad> resultadoEntidadConsultada = ConsultarXId();
-                if (resultadoEntidadConsultada.Exito)
-				{
-                    TEntidad entidadConsultada = resultadoEntidadConsultada.Value;
-                    TEntidad entidadEliminada = DJYM.Set<TEntidad>().Remove(entidadConsultada);
-					DJYM.SaveChanges();
+                string mensajeExito = $"{typeof(TEntidad).Name} se actualizó exitosamente";
+                return new Resultado<TEntidad>(Entidad) { MensajeExito = mensajeExito };
+            }
+            catch (Exception ex)
+            {
+                return new Resultado<TEntidad>(ex.Message);
+            }
+        }
 
-					string mensajeExito = $"{typeof(TEntidad).Name} se elimina exitosamente";
-					Resultado<TEntidad> resultado = new Resultado<TEntidad>(entidadEliminada)
-					{
-						MensajeExito = mensajeExito
-					};
-					return resultado;
-				}
-				else
-				{
-					string mensajeError = $"{typeof(TEntidad).Name} con identificador {Entidad.ClavePrimaria} no existe en la base de datos";
-					return new Resultado<TEntidad>(mensajeError);
-				}
-			}
-			catch (Exception ex)
-			{
-				return new Resultado<TEntidad>(ex.Message);
-			}
-		}
+        public Resultado<TEntidad> Eliminar()
+        {
+            try
+            {
+                Resultado<TEntidad> resultado = ConsultarXId();
+                if (!resultado.Exito)
+                    return resultado;
+
+                DJYM.Set<TEntidad>().Remove(resultado.Value);
+                DJYM.SaveChanges();
+
+                string mensajeExito = $"{typeof(TEntidad).Name} se eliminó exitosamente";
+                return new Resultado<TEntidad>(Entidad) { MensajeExito = mensajeExito };
+            }
+            catch (Exception ex)
+            {
+                return new Resultado<TEntidad>(ex.Message);
+            }
+        }
     }
 }
